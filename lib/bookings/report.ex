ea0c0a42d36caa95dcd2048f0e55332ff10e28db
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-03-20 15:34:08 -0300

    Falta separar por data inicial e final

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -2,18 +2,34 @@ defmodule FlightReservation.Bookings.Report do
   alias FlightReservation.Bookings.Agent, as: BookingAgent
   alias FlightReservation.Bookings.Booking
 
-  def create(filename) do
+  def create(filename, from_date, to_date) do
     bookings_list = build_bookings_list()
-    File.write("reports/"<>filename, bookings_list)
+    file_name = from_date <> "_" <> to_date <> "_" <> filename
+    File.write("reports/" <> file_name, bookings_list)
   end
 
   defp build_bookings_list() do
-    BookingAgent.get_all
+    BookingAgent.get_all()
     |> Map.values()
     |> Enum.map(fn booking -> booking_string(booking) end)
+
+
   end
 
-  defp booking_string(%Booking{cidade_origem: cidade_origem, cidade_destino: cidade_destino}) do
-    cidade_origem <> ", " <> cidade_destino <> "\n"
+  defp booking_string(%Booking{
+         cidade_origem: cidade_origem,
+         cidade_destino: cidade_destino,
+         id_usuario: id_usuario,
+         data_completa: data_completa
+       }) do
+    id_usuario.id <>
+      "," <>
+      cidade_origem <>
+      "," <>
+      cidade_destino <>
+      "," <>
+      date_format(data_completa) <> "\n"
   end
+
+  defp date_format(data_completa), do: NaiveDateTime.to_string(data_completa)
 end
